eeb6cf285c1169b529e8c309594b9f0603cf3a86
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-07 02:36:18 +0300

    Finalized auth and backend setup

---
+++ server.js
@@ -0,0 +1,42 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const PORT = process.env.PORT || "3000";
+
+// CONTROLLERS HERE
+const authCtrl = require("./controllers/auth-controller");
+
+
+// MIDDLEWARES HERE
+const verifyToken = require("./middleware/verify-toke");
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// AUTHENTICATION ROUTES
+app.post("/auth/sign-up", authCtrl.signUp);
+app.post("/auth/sign-in", authCtrl.signIn);
+
+
+// MONGO CONNECTION
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`Connected to MongoDB ${mongoose.connection.name}. 🥭`);
+        app.listen(PORT, () => {
+            console.log(`The express app is ready on port ${PORT}! 😀`);
+        });
+    } catch (e) {
+        console.log("Error Message:", e.message)
+    }
+}
+
+startServer();
