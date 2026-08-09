0af0c26e5fc6acf3c8870fe67907f48c620bc5ae
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-09 04:21:16 +0300

    Add helper function to normalize the response data

--- services/nutritionService.js
+++ services/nutritionService.js
@@ -7,9 +7,32 @@ const searchFoods = async (query) => {
 
     const data = await res.json();
 
-    return data;
+    return data.foods.map(normalizedSearchFood); // shortcut instead of writing arrow function
+
 }
 
 module.exports = {
     searchFoods,
+}
+
+// Helper function instead of me writing .find() 4 times
+function getNutrientValues(nutrients, nutrientId) {
+    const nutrient = nutrients.find(nutrient => nutrient.nutrientId === nutrientId);
+
+    return nutrient ? nutrient.value : null;
+}
+
+// Normalized data that match my needs
+function normalizedSearchFood(food) {
+    return {
+        externalId: String(food.fdcId),
+        name: food.description,
+        brand: food.brandOwner || null,
+        dataType: food.dataType,
+
+        calories: getNutrientValues(food.foodNutrients, 1008),
+        protein: getNutrientValues(food.foodNutrients, 1003),
+        carbohydrates: getNutrientValues(food.foodNutrients, 1005),
+        fat: getNutrientValues(food.foodNutrients, 1004),
+    }
 }
